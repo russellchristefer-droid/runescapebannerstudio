c5e54a442743b0b444b091e335233f8f5d2eedeb
Christefer Lee Russell-Barnett <russell.christefer@gmail.com>
2026-09-22 08:47:45 +0000

    Export GAME_NAV with the split rail.

--- src/places/index.ts
+++ src/places/index.ts
@@ -1,4 +1,4 @@
 export { PlaceRail, usePlaceFilter, type PlaceSection } from "./place-rail";
-export { STUDIO_NAV, STUDIO_INDEX, PLACE_SECTIONS } from "./nav";
+export { GAME_NAV, STUDIO_NAV, STUDIO_INDEX, PLACE_SECTIONS } from "./nav";
 export { PlaceCard, PlaceGrid } from "./place-card";
 export { PlaceChip, VisitPlaces, AppLink, godPath, townPath, bossPath } from "./place-chip";
